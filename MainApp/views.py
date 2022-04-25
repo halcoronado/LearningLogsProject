a42512aa07fd4f3ac61d3fa35f54fc3e6c279f42
Hallie Coronado <hallie_coronado1@baylor.edu>
2022-04-25 15:46:00 -0500

    asfd

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
+def index(request):
+    return render(request, 'MainApp/index.html')
+
+def topics(requests):
+    topics = Topic.objects.all()
+
+    context = {'topics': topics}
+
+    return render(requests, 'MainApp/topics.html', context)
+
