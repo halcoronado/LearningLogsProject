1ce03338adbfc678a99d0287765f02fc413f8301
Hallie Coronado <hallie_coronado1@baylor.edu>
2022-05-04 14:33:37 -0500

    fdgdf

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 import imp
+from django import views
 from django.urls import path, include
+from . import views
 
 app_name ='users'
 
 urlpatterns=[
     path('', include('django.contrib.auth.urls')),
-
+    path('register/', views.register, name="register"),
 ]
